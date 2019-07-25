be4852136ce627826761ab9875615dd18929761a
utkarsh <anandutkarsh39@gmail.com>
2019-07-25 09:49:15 +0530

    created global exception

--- src/main/java/com/stackroute/userService/TrackServiceApplication.java
+++ src/main/java/com/stackroute/userService/TrackServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrackServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(TrackServiceApplication.class, args);
 	}
 
