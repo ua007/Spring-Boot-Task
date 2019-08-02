cf798977d71bf150aa04b5b8cb7dbd0cafcc4c61
utkarsh <anandutkarsh39@gmail.com>
2019-08-02 15:08:07 +0530

    SpringBoot-Assignment1-WithTestCases

---
+++ src/main/java/com/stackroute/userService/TrackServiceApplication.java
@@ -0,0 +1,16 @@
+package com.stackroute.userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackServiceApplication {
+
+	public static void main(String[] args){
+//		JsonObject jsonObject =new JsonObject();
+//		jsonObject.JsonObject();
+		SpringApplication.run(TrackServiceApplication.class, args);
+
+	}
+
+}
